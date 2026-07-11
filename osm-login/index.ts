3d470e459c9ea7ea0631f9730032dd2a1382b40e
strale1988 <strale.com@gmail.com>
2026-07-12 00:17:52 +0200

    Change import source for Supabase client

--- osm-login/index.ts
+++ osm-login/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'npm:@supabase/supabase-js@2';
 
 const OSM_AUTH_URL = 'https://www.openstreetmap.org/oauth2/authorize';
 const OSM_TOKEN_URL = 'https://www.openstreetmap.org/oauth2/token';
