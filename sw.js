53b00bc01a8288f7a90cff6c32a8c5edfc374943
strale1988 <strale.com@gmail.com>
2026-08-02 18:15:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -80,3 +80,18 @@ self.addEventListener('fetch', event => {
       .catch(() => caches.match(req).then(cached => cached || caches.match(APP_SHELL_URL)))
   );
 });
+
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {};
+  event.waitUntil(self.registration.showNotification(data.title || 'TraceTheBreak', {
+    body: data.body || '',
+    icon: './icons/TraceTheBreak.png',
+    data: { url: data.url || '/' },
+  }));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  const url = event.notification.data?.url || '/';
+  event.waitUntil(clients.openWindow(url));
+});
